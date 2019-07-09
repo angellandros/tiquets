0a8a13fd3b36023ecc77996377598d1ee7efee14
Muhammad-Ali A'rabi <angellandros@yahoo.com>
2019-07-09 21:02:48 +0200

    Add log for unassigned barcodes, and fix randomness of doctests

--- tiquets.py
+++ tiquets.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 import argparse
 import csv
 from collections import defaultdict
@@ -23,8 +25,11 @@ def load_barcodes(barcodes_file):
     :param barcodes: CSV file containing data of barcode and order_id
     :return: a map from order_ids into barcodes, and a list of unassigned barcodes
 
-    >>> load_barcodes(open('barcodes_test.csv'))
-    ({'1': ['11111111111'], '2': ['11111111112']}, ['11111111113'])
+    >>> inverted_index, unassigned_barcodes = load_barcodes(open('barcodes_test.csv'))
+    >>> sorted(inverted_index.items(), key=lambda x: x[0])
+    [('1', ['11111111111']), ('2', ['11111111112'])]
+    >>> unassigned_barcodes
+    ['11111111113']
     """
     inverted_index = defaultdict(list)
     assigned_barcodes = dict()
@@ -70,8 +75,10 @@ def make_complete_report(orders_data, barcodes_index, output_file):
 
 def main(orders_file, barcodes_file, output_file):
     orders_data = load_orders(orders_file)
-    barcodes_index, _ = load_barcodes(barcodes_file)
+    barcodes_index, unassigned_barcodes = load_barcodes(barcodes_file)
     make_complete_report(orders_data, barcodes_index, output_file)
+    # log unused barcodes
+    print('unused barcodes:', ', '.join(unassigned_barcodes))
 
 
 if __name__ == '__main__':
